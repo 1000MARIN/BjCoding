c12cd4a20444c9c0b0b8f3ca70fa5a36bc7fef86
1000marin <leexjin@gmail.com>
2022-05-26 14:07:49 +0900

    bj/java

---
+++ src/Bj2/Compare.java
@@ -0,0 +1,25 @@
+package Bj2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Compare {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = br.readLine().split(" ");
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
+
+        br.close();
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
