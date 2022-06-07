060105657c58db8811d9e7b347d7c94a724b3df7
1000marin <leexjin@gmail.com>
2022-06-07 10:44:47 +0900

    Bj/Java

---
+++ src/Bj3/Test.java
@@ -0,0 +1,17 @@
+package Bj3;
+
+public class Test {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <=i; j++) {
+				sb.append("*");
+			}
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
